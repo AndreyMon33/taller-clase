80ea0edbfdef8c98bb9260f75ee0118313c19bfa
Andrey <andrey2015monsalve@gmail.com>
2025-11-13 11:38:05 -0500

    feat se agrega la funcion para que se agreguen 3 precios de articulos y puedas visualizar la suma de estos

---
+++ AndreyMonsalveVilla/andreyJS.js
@@ -0,0 +1,7 @@
+let articulo1 = prompt("Ingrese el precio del primer articulo:");
+let articulo2 = prompt("Ingrese el precio del segundo articulo:");
+let articulo3 = prompt("Ingrese el precio del tercer articulo:");
+
+let resultado = parseInt(articulo1) + parseInt(articulo2) + parseInt(articulo3);
+
+console.log("Resultado total de los articulos:", resultado);
