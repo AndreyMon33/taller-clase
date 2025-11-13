7690e001e0d5880011e6025e60fc946fa7abaf2b
Andrey <andrey2015monsalve@gmail.com>
2025-11-13 12:01:04 -0500

    feat se agrega la funcion para tomar el ancho y alto de una pared para brindar el area

--- AndreyMonsalveVilla/andreyJS.js
+++ AndreyMonsalveVilla/andreyJS.js
@@ -1,3 +1,5 @@
+/*Ejercicio 1*/
+
 let articulo1 = prompt("Ingrese el precio del primer articulo:");
 let articulo2 = prompt("Ingrese el precio del segundo articulo:");
 let articulo3 = prompt("Ingrese el precio del tercer articulo:");
@@ -5,3 +7,12 @@ let articulo3 = prompt("Ingrese el precio del tercer articulo:");
 let resultado = parseInt(articulo1) + parseInt(articulo2) + parseInt(articulo3);
 
 console.log("Resultado total de los articulos:", resultado);
+
+/*Ejercisio 2*/
+
+let anchoPared = prompt("Ingrese el ancho de la pared en metros:");
+let largoPared = prompt("Ingrese el alto de la pared en metros:");
+
+let areaPared = parseFloat(anchoPared) * parseFloat(largoPared);
+
+console.log("El area de la pared es de:", areaPared);
